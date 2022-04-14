e183b12a428eeccddf248a37dbe27b045000ec06
jitty-code <sandicorevery@gmail.com>
2022-04-15 01:33:44 +0500

    left the test in there :|

--- test.js
+++
@@ -1,8 +0,0 @@
-const story = require("./index")
-
-const myStory = new story({
-    start: {message: "What is your name?", type: "prompt", options: ["name"], next: "greet"},
-    greet: {message: "Hello, $name"}
-})
-
-myStory.start(true)
